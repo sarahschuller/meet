b5a2180064a12be30bf753825b91200454b2a918
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-02 18:59:17 -0500

    create CitySearch and import in app and app.test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -2,19 +2,24 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
 import EventList from '../EventList';
+import CitySearch from '../CitySearch';
+
 import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App /> component', () => {
+  let AppWrapper;
+  beforeAll(() => {
+    AppWrapper = shallow(<App />);
+  });
+
   test('render list of events', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(EventList)).toHaveLength(1);
   });
 
   test('render CitySearch', () => {
-    const AppWrapper = shallow(<App />);
     expect(AppWrapper.find(CitySearch)).toHaveLength(1);
   });
 });
