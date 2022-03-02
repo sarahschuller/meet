75b6520fb08acaebe15c4408ef664e6a224f8071
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-02 18:46:15 -0500

    add eventlist.test, update eventlist and app tests. add babelrc

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from '../App';
+import EventList from '../EventList';
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('<App /> component', () => {
-    test('render list of events', () => {
-        const AppWrapper = shallow(<App />);
-        expect(AppWrapper.find(EventList)).toHaveLength(1);
-      });
+  test('render list of events', () => {
+    const AppWrapper = shallow(<App />);
+    expect(AppWrapper.find(EventList)).toHaveLength(1);
+  });
 });
