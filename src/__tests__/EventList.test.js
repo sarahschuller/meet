0e1b27663ed353eb34fab0d391dd3f7c09a57d79
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-05 15:57:43 -0500

    update render number of events test to use mockdata

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -9,7 +9,10 @@ import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 Enzyme.configure({ adapter: new Adapter() });
 
 describe('<EventList /> component', () => {
+
   test('render correct number of events', () => {
-    const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
+    const EventListWrapper = shallow(<EventList events={mockData} />);
+    expect(EventListWrapper.find(Event)).toHaveLength(mockData.length);
   });
+
 });
