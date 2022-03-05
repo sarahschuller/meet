ad72291744b8cf645d8864bbb8d7046fbe6dd97f
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-05 15:56:31 -0500

    import enzyme and adapter

--- src/__tests__/EventList.test.js
+++ src/__tests__/EventList.test.js
@@ -3,6 +3,11 @@ import { shallow } from 'enzyme';
 import EventList from '../EventList';
 import Event from '../Event';
 
+import Enzyme from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+
+Enzyme.configure({ adapter: new Adapter() });
+
 describe('<EventList /> component', () => {
   test('render correct number of events', () => {
     const EventListWrapper = shallow(<EventList events={[{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }]} />);
