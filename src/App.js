2edc67a7f334892688a73e1d42acbccbf51aea97
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-06 18:54:56 -0500

    pass events state to EventList as a prop

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
 
 class App extends Component {
-  
+
   state ={
     events: []
   }
@@ -14,7 +14,7 @@ class App extends Component {
     return (
       <div className="App">
         <CitySearch />
-        <EventList />
+        <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
     );
