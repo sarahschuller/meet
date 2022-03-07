8c87d4b920855a6603fdffb190afe13ee8d54db8
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-06 19:00:35 -0500

    pass locations state as prop to CitySearch

--- src/App.js
+++ src/App.js
@@ -7,13 +7,14 @@ import NumberOfEvents from './NumberOfEvents';
 class App extends Component {
 
   state ={
-    events: []
+    events: [],
+    locations: []
   }
 
   render() {
     return (
       <div className="App">
-        <CitySearch />
+        <CitySearch locations={this.state.locations} />
         <EventList events={this.state.events} />
         <NumberOfEvents />
       </div>
