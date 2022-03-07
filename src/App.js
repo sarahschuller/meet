d6842b5eeaa52c4d9b174e51b61872c9babb02fe
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-06 19:17:44 -0500

    pass updateEvents method as prop to CitySearch

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <CitySearch locations={this.state.locations} />
-        <EventList events={this.state.events} />
-        <NumberOfEvents />
+      <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />        
+      <EventList events={this.state.events} />
+      <NumberOfEvents />
       </div>
     );
   }
