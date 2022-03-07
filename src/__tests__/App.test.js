ff03db68997e557654e31bd768e7b969448192b3
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-06 19:07:24 -0500

    add test to get list of events matching the city selected by the user

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -51,4 +51,19 @@ describe('<App /> integration', () => {
     AppWrapper.unmount();
   });
 
+  test('get list of events matching the city selected by the user', async () => {
+    const AppWrapper = mount(<App />);
+    const CitySearchWrapper = AppWrapper.find(CitySearch);
+    const locations = extractLocations(mockData);
+    CitySearchWrapper.setState({ suggestions: locations });
+    const suggestions = CitySearchWrapper.state('suggestions');
+    const selectedIndex = Math.floor(Math.random() * (suggestions.length));
+    const selectedCity = suggestions[selectedIndex];
+    await CitySearchWrapper.instance().handleItemClicked(selectedCity);
+    const allEvents = await getEvents();
+    const eventsToShow = allEvents.filter(event => event.location === selectedCity);
+    expect(AppWrapper.state('events')).toEqual(eventsToShow);
+    AppWrapper.unmount();
+  });
+
 });
