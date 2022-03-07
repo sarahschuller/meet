a890af60e9feafa9b4d02c03103d2dc73a2a974c
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-06 19:12:31 -0500

    import mockData and export getEvents

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
+import { mockData } from './mock-data';
+
+export const getEvents = async () => {
+  return mockData;
+};
 
 class App extends Component {
 
