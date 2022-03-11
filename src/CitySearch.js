03c19c60ba136d72baa15c2f10fde09508862370
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-11 18:23:03 -0500

    add showSuggestion state

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 class CitySearch extends Component {
   state = {
     query: '',
-    suggestions: []
+    suggestions: [],
+    showSuggestions: undefined,
   }
 
   handleInputChanged = (event) => {
