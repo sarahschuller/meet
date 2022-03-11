d539ee23c626e6847285ae8f46779eca1e6e9c83
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-11 18:05:54 -0500

    re-arrange order of code, remove unneeded import and export

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { mockData } from './mock-data';
 import { getEvents, extractLocations } from './api';
 
-export const getEvents = async () => {
-  return mockData;
-};
+class App extends Component {
+  state = {
+    events: [],
+    locations: [],
+  }
 
 updateEvents = (location) => {
   getEvents().then((events) => {
@@ -31,19 +32,19 @@ componentWillUnmount(){
   this.mounted = false;
 }
 
-class App extends Component {
-
-  state ={
-    events: [],
-    locations: []
-  }
-
   render() {
     return (
       <div className="App">
-      <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />        
-      <EventList events={this.state.events} />
+
+      <CitySearch 
+      locations={this.state.locations} 
+      updateEvents={this.updateEvents} />  
+
+      <EventList 
+      events={this.state.events} />
+
       <NumberOfEvents />
+
       </div>
     );
   }
