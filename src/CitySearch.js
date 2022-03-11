d385aed1717b5c1362f3c056700897b6fee4ca7e
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-11 18:24:56 -0500

    add event listener for focus event on city input field

--- src/CitySearch.js
+++ src/CitySearch.js
@@ -34,6 +34,7 @@ class CitySearch extends Component {
           className="city"
           value={this.state.query}
           onChange={this.handleInputChanged}
+          onFocus={() => { this.setState({ showSuggestions: true }) }}
         />
         <ul className="suggestions">
           {this.state.suggestions.map((suggestion) => (
