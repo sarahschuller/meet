f8d4ef5ad3d11f0e8acd786759373b5a582dd7d2
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-11 19:30:50 -0500

    const checkToken

--- src/api.js
+++ src/api.js
@@ -15,6 +15,16 @@ import axios from 'axios';
     return locations;
   };
 
+  const checkToken = async (accessToken) => {
+    const result = await fetch(
+      `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${accessToken}`
+    )
+      .then((res) => res.json())
+      .catch((error) => error.json());
+  
+    return result;
+  };
+
   export const getEvents = async () => {
     return mockData;
   };
@@ -22,4 +32,25 @@ import axios from 'axios';
   export const getAccessToken = async () =>
   {
     const accessToken = localStorage.getItem('access_token');
+    const tokenCheck = accessToken && (await checkToken(accessToken));
+
+    if (!accessToken || tokenCheck.error) {
+      await localStorage.removeItem("access_token");
+      const tokenCheck = accessToken && (await checkToken(accessToken));
+
+      if (!accessToken || tokenCheck.error) {
+        await localStorage.removeItem("access_token");
+        const searchParams = new URLSearchParams(window.location.search);
+        const code = await searchParams.get("code");
+        if (!code) {
+          const results = await axios.get(
+            "https://1k6hw3ll5h.execute-api.us-east-1.amazonaws.com/dev/api/get-auth-url"
+          );
+          const { authUrl } = results.data;
+          return (window.location.href = authUrl);
+        }
+        return code && getToken(code);
+      }
+      return accessToken;
+    }
   };
