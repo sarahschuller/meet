eb4a3820bd7dc8b48030da6a8a0416cda94a735e
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-16 18:24:07 -0400

    troubleshooting

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,39 +1,28 @@
 import React, { Component } from 'react';
 
 class NumberOfEvents extends Component {
-     state = {
-         numberOfEvents: '30',
-         infoText: "",
-       }
+	state = {
+		numberOfEvents: '32',
+	}
 
-       handleInputChanged = (event) => {
-        const value = event.target.value;
-        if (value < 1 || value > 30) {
-          this.setState({
-            numberOfEvents: "",
-            infoText: "Please enter a number between 1 and 30",
-          });
-        } else {
-          this.setState({
-            numberOfEvents: value,
-            infoText: "",
-          });
-        }
-        this.props.updateNumberOfEvents(event.target.value);
-      };
+	handleInputChange = (event) => {
+		const value = event.target.value;
+		this.setState({
+			numberOfEvents: event.target.value,
+		});
+	}
 
     render() {
-      return(
-      <div className="NumberOfEvents">
-        <p>Number of Events</p>
-          <input 
-          type="number" 
-          className="numberOfEvents"
-          value={this.state.numberOfEvents} 
-          onChange={this.handleInputChange}
-          />
-      </div>
+      return (  
+        <div className='NumberOfEvents'>
+            <input
+						type="number"
+						onChange={this.handleInputChange} 
+						value={this.state.numberOfEvents} 
+						className="numberOfEvents"/>
+        </div>
+			)	
+    }
+};    
 
-      )
- }};
 export default NumberOfEvents;
--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,28 +1,21 @@
-import React from "react";
-import { shallow } from "enzyme";
-import NumberOfEvents from "../NumberOfEvents";
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
 
-import Enzyme from 'enzyme';
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+describe('<NumberOfEvents /> component', () => {
+    let NumberOfEventsWrapper;
 
-Enzyme.configure({ adapter: new Adapter() });
-
-describe("<NumberOfEvents /> component", () => {
-  let NumberOfEventsWrapper;
-
-  beforeAll(() => {
-    NumberOfEventsWrapper = shallow(<NumberOfEvents />);
-  });
-
-  test("render text input", () => {
-    expect(NumberOfEventsWrapper.find(".numberOfEvents")).toHaveLength(1);
-  });
-
-  test("change state when number input changes", () => {
-    NumberOfEventsWrapper.setState({ numberOfEvents: "30" });
-    NumberOfEventsWrapper.find(".numberOfEvents").simulate("change", {
-      target: { value: "10" },
+    beforeAll(() => {
+        NumberOfEventsWrapper = shallow(<NumberOfEvents />)
     });
-    expect(NumberOfEventsWrapper.state("numberOfEvents")).toEqual("10");
-  });
-});
+
+		test('render text input', () => {
+			expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+		});
+
+		test('change numberOfEvents state when number input changes', () => {
+			NumberOfEventsWrapper.setState({numberOfEvents: '32'});
+			NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', {target: {value: "11"}});
+			expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual("11");
+		});
+})
