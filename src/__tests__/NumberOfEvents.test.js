6615fb132ed0e2340f9a708c64bb344ed5070f69
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-16 18:28:48 -0400

    troubleshooting

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import NumberOfEvents from '../NumberOfEvents';
+import React, { Component } from "react";
+import { ErrorAlert } from "./Alert";
 
-describe('<NumberOfEvents /> component', () => {
-    let NumberOfEventsWrapper;
+class NumberOfEvents extends Component {
+  state = {
+    numberOfEvents: 32,
+    infoText: "",
+  };
 
-    beforeAll(() => {
-        NumberOfEventsWrapper = shallow(<NumberOfEvents />)
-    });
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value < 1 || value > 32) {
+      this.setState({
+        numberOfEvents: "",
+        infoText: "Please enter a number between 1 and 32",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        infoText: "",
+      });
+    }
+    this.props.updateNumberOfEvents(event.target.value);
+  };
 
-		test('render text input', () => {
-			expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
-		});
+  render() {
+    return (
+      <div className="NumberOfEvents">
+        <br></br>
+        <p>Number of Events:</p>
+        <input
+          type="number"
+          className="numberOfEvents"
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
 
-		test('change numberOfEvents state when number input changes', () => {
-			NumberOfEventsWrapper.setState({numberOfEvents: '32'});
-			NumberOfEventsWrapper.find('.numberOfEvents').simulate('change', {target: {value: "11"}});
-			expect(NumberOfEventsWrapper.state('numberOfEvents')).toEqual("11");
-		});
-})
+        <ErrorAlert text={this.state.infoText} />
+      </div>
+    );
+  }
+}
+export default NumberOfEvents;
