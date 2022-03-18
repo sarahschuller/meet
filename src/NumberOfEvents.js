6ec517582cbc7fa55c730e333e38d79ee672b55b
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-18 17:32:58 -0400

    update

--- src/NumberOfEvents.js
+++ src/NumberOfEvents.js
@@ -1,39 +1,41 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class NumberOfEvents extends Component {
-     state = {
-         numberOfEvents: '30',
-         infoText: "",
-       }
+  state = {
+    numberOfEvents: 30,
+    infoText: "",
+  };
 
-       handleInputChanged = (event) => {
-        const value = event.target.value;
-        if (value < 1 || value > 30) {
-          this.setState({
-            numberOfEvents: "",
-            infoText: "Please enter a number between 1 and 30",
-          });
-        } else {
-          this.setState({
-            numberOfEvents: value,
-            infoText: "",
-          });
-        }
-        this.props.updateNumberOfEvents(event.target.value);
-      };
+  handleInputChanged = (event) => {
+    const value = event.target.value;
+    if (value < 1 || value > 30) {
+      this.setState({
+        numberOfEvents: "",
+        infoText: "Please enter a number between 1 and 30",
+      });
+    } else {
+      this.setState({
+        numberOfEvents: value,
+        infoText: "",
+      });
+    }
+    this.props.updateNumberOfEvents(event.target.value);
+  };
 
-    render() {
-      return(
+  render() {
+    return (
       <div className="NumberOfEvents">
-        <p>Number of Events</p>
-          <input 
-          type="number" 
+        <br></br>
+        <p>Number of Events:</p>
+        <input
+          type="number"
           className="numberOfEvents"
-          value={this.state.numberOfEvents} 
-          onChange={this.handleInputChange}
-          />
-      </div>
+          value={this.state.numberOfEvents}
+          onChange={this.handleInputChanged}
+        />
 
-      )
- }};
+      </div>
+    );
+  }
+}
 export default NumberOfEvents;
