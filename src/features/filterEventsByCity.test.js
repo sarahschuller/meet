451ea057adf09245e77adbd0997432a5b455a0fe
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-18 18:35:05 -0400

    add test - city should be changed to that city

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -60,7 +60,8 @@ defineFeature(feature, test => {
       });
   
       then('their city should be changed to that city (i.e., “Berlin, Germany”)', () => {
-  
+        const CitySearchWrapper = AppWrapper.find(CitySearch);
+        expect(CitySearchWrapper.state('query')).toBe('Berlin, Germany');
       });
   
       and('the user should receive a list of upcoming events in that city', () => {
