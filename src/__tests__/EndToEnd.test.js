836d2214438cf39016e06ebb4b7f130f8f71c7ca
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-18 18:49:20 -0400

    import puppeteer

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -1,4 +1,4 @@
-import { puppeteerErrors } from "puppeteer";
+import puppeteer from 'puppeteer';
 
 describe('show/hide an event details', () => {
     test('An event element is collapsed by default', async () => {
