ecedba7b39320533ea537a94a34f910fd4d92696
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-18 18:23:53 -0400

    import extractLocations

--- src/features/filterEventsByCity.test.js
+++ src/features/filterEventsByCity.test.js
@@ -4,7 +4,7 @@ import { mount, shallow } from 'enzyme';
 import App from '../App';
 import { mockData } from '../mock-data';
 import CitySearch from '../CitySearch';
-
+import { extractLocations } from "../api";
 
 const feature = loadFeature('./src/features/filterEventsByCity.feature');
 
