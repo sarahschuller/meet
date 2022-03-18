624131eac095cbc61fbae347250642a81131459f
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-18 18:56:30 -0400

    add test - user can collapse an event to hide details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -25,6 +25,12 @@ describe('show/hide an event details', () => {
       const eventDetails = await page.$('.event .event__Details');
       expect(eventDetails).toBeDefined();
     });
+    
+    test('User can collapse an event to hide its details', async () => {
+        await page.click('.event .details-btn');
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeNull();
+      });
   });
   
   
