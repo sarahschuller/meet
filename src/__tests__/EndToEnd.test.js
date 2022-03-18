05efb8ed32ac9d7ec4fcdc5b1877e9e611dbf159
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-18 18:53:47 -0400

    add test - user can expand an event to see its details

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -4,7 +4,7 @@ describe('show/hide an event details', () => {
     beforeAll(async () => {
         jest.setTimeout(30000);
     });
-    
+
     test('An event element is collapsed by default', async () => {
         const browser = await puppeteer.launch();
     
@@ -15,6 +15,19 @@ describe('show/hide an event details', () => {
     
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeNull();
+        browser.close();
+      });
+
+      test('User can expand an event to see its details', async () => {
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('http://localhost:3000/');
+    
+        await page.waitForSelector('.event');
+        await page.click('.event .details-btn');
+    
+        const eventDetails = await page.$('.event .event__Details');
+        expect(eventDetails).toBeDefined();
         browser.close();
       });
 });
