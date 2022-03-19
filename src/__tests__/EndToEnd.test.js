90f12c8647cf83bf6d86fe8b3de47f2c312608a5
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-19 16:41:14 -0400

    update class name to match info in event.js

--- src/__tests__/EndToEnd.test.js
+++ src/__tests__/EndToEnd.test.js
@@ -12,7 +12,7 @@ describe('show/hide an event details', () => {
       });
       page = await browser.newPage();
       await page.goto('http://localhost:3000/');
-      await page.waitForSelector('.event');
+      await page.waitForSelector('.event', {timeout: 30000});
     });
   
     afterAll(() => {
@@ -25,13 +25,13 @@ describe('show/hide an event details', () => {
     });
   
     test('User can expand an event to see its details', async () => {
-      await page.click('.event .details-btn');
+      await page.click('.event .details-button');
       const eventDetails = await page.$('.event .event__Details');
       expect(eventDetails).toBeDefined();
     });
     
     test('User can collapse an event to hide its details', async () => {
-        await page.click('.event .details-btn');
+        await page.click('.event .details-button');
         const eventDetails = await page.$('.event .event__Details');
         expect(eventDetails).toBeNull();
       });
