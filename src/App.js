861d92916bccaaef79792f55accbacafd3b006bf
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-26 15:37:16 -0400

    add WelcomeScreen

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import './nprogress.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, getAccessToken } from './api';
 import { OfflineAlert } from './Alert';
+import WelcomeScreen from './WelcomeScreen';
 import {
   ScatterChart, 
   Scatter, 
@@ -92,6 +93,25 @@ class App extends Component {
   };
 
   render() {
+    const refreshPage = () => {
+      window.location.reload();
+    }
+
+    if (this.state.showWelcomeScreen === undefined &&
+      navigator.onLine && !window.location.href.startsWith('http://localhost')
+    ) {
+      return <div className='App' />;
+    }
+    if (this.state.showWelcomeScreen === true)
+      return (
+        <WelcomeScreen
+          showWelcomeScreen={this.state.showWelcomeScreen}
+          getAccessToken={() => {
+            getAccessToken();
+          }}
+        />
+      );
+
     const { locations, numberOfEvents, OfflineAlertText } = this.state;
     return (
       <div className="App">
@@ -142,6 +162,11 @@ class App extends Component {
         
         <EventList 
         events={this.state.events} />
+
+        <WelcomeScreen 
+        showWelcomeScreen={this.state.showWelcomeScreen}
+        getAccessToken={() => { getAccessToken() }} />
+
       </div>
     );
   }
