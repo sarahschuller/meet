f7efb016723d161eb5bfec6c207f3ac6621cea73
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-26 12:44:05 -0400

    import responsive container

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import {
   XAxis, 
   YAxis, 
   CartesianGrid, 
-  Tooltip
+  Tooltip,
+  ResponsiveContainer
 } from 'recharts';
 
 class App extends Component {
@@ -103,15 +104,15 @@ class App extends Component {
          <h4>Events in each city</h4>
 
          <ScatterChart
-          width={400}
+          width={800}
           height={400}
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
           }}
         >
           <CartesianGrid />
-          <XAxis type="category" dataKey="city" name="city" />
-          <YAxis type="number" dataKey="number" name="number of events" />
+          <XAxis type="category" dataKey="city" name="City" />
+          <YAxis type="number" dataKey="number" name="Number of Events" allowDecimals={false} />
           <Tooltip cursor={{ strokeDasharray: '3 3' }} />
           <Scatter data={this.getData()} fill="#8884d8" />        </ScatterChart>
         <EventList events={this.state.events} />
