bfd12756a6a5f23ac9a9076ff0533b8384e1e578
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-26 15:20:17 -0400

    update

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,2 +1,11 @@
 import React from "react";
 import './WelcomeScreen.css';
+
+function WelcomeScreen(props) {
+    return props.showWelcomeScreen ?
+      (
+        <div className="WelcomeScreen">
+          <h1>Welcome to the Meet App!</h1>
+          <h3>
+            Login to view upcoming events for web developers around the world!
+          </h3>
