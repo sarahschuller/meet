8cfa3827d677f23f0773e28bea1d895247e3398c
sarahschuller <93050611+sarahschuller@users.noreply.github.com>
2022-03-26 13:41:15 -0400

    add classname to scatterchart

--- src/App.js
+++ src/App.js
@@ -105,6 +105,7 @@ class App extends Component {
 
         <ResponsiveContainer height={400}>
          <ScatterChart
+          className="chart"
           margin={{
             top: 20, right: 20, bottom: 20, left: 20,
           }}
